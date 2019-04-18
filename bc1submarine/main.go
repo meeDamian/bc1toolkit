43b2db048bf503e5ed040b7271577832dd0aab91
Damian Mee <spam@meedamian.com>
2019-04-18 17:23:21 +0700

    initial take on submarine swaps

---
+++ bc1submarine/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/lightningnetwork/lnd/input"
+	"github.com/meeDamian/bc1toolkit/lib/common"
+	"github.com/meeDamian/bc1toolkit/lib/help"
+)
+
+const (
+	BinaryName = "bc1submarine"
+
+	description = `Constructs and returns a Bitcoin submarine atomic swap script.
+
+Setup an atomic swap unlocking BTC/on-chain payment, only upon successful LN/off-chain payment.`
+)
+
+type Swap struct {
+	MyPubKey    btcec.PublicKey
+	TheirPubKey btcec.PublicKey
+	Hash        string
+	Height      int64
+}
+
+var (
+	opts struct {
+		TestNet bool   `long:"testnet" short:"T" description:"Uses testnet addresses.  Connects to testnet nodes."`
+		PubKey  string `long:"pubkey" description:""`
+		Bitcoin string `long:"bitcoin" description:"Bitcoin's RPC interface" default:"localhost:8332" default:"localhost:18332" default-mask:"localhost:8332, or localhost:18332 for testnet"`
+
+		Lnd struct {
+			Host     string `long:"host" description:"Lnd's RPC interface'" default:"localhost:10009"`
+			Cert     string `long:"cert" description:"TLS certificate that LND uses to auth RPC connections'" default-mask:"~/.lnd/tls.cert"`
+			Macaroon string `long:"macaroon" description:"Path to admin.macaroon lnd generated.  See description for \"why admin?\"" default:"~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon" default:"~/.lnd/data/chain/bitcoin/testnet/admin.macaroon" default-mask:"~/.lnd/data/chain/bitcoin/(main|test)net/admin.macaroon"`
+		} `namespace:"lnd" group:"lnd"`
+	}
+	commonOpts help.Opts
+
+	swap Swap
+)
+
+func init() {
+	common.Logger.Name(BinaryName)
+
+	help.Customize(
+		"[OPTIONS] their-pubkey [invoice|hash] [your-pubkey] [height]",
+		description,
+		help.DisableTor,
+		BinaryName, &opts,
+	)
+
+	swap = Swap{}
+
+	// read parameters passed to a binary
+	var args []string
+	args, commonOpts = help.Parse()
+
+	if len(args) == 0 {
+		fmt.Println("At least the pubkey of redeemer has to be provided")
+		os.Exit(1)
+	}
+}
+
+func genSubmarineSwapScript(swapperPubKey, payerPubKey, hash []byte, lockHeight int64) ([]byte, error) {
+	builder := txscript.NewScriptBuilder()
+
+	builder.AddOp(txscript.OP_HASH160)
+	builder.AddData(input.Ripemd160H(hash))
+	builder.AddOp(txscript.OP_EQUAL) // Leaves 0P1 (true) on the stack if preimage matches
+	builder.AddOp(txscript.OP_IF)
+	builder.AddData(swapperPubKey) // Path taken if preimage matches
+	builder.AddOp(txscript.OP_ELSE)
+	builder.AddInt64(lockHeight)
+	builder.AddOp(txscript.OP_CHECKSEQUENCEVERIFY)
+	builder.AddOp(txscript.OP_DROP)
+	builder.AddData(payerPubKey) // Refund back to payer
+	builder.AddOp(txscript.OP_ENDIF)
+	builder.AddOp(txscript.OP_CHECKSIG)
+
+	return builder.Script()
+}
+
+
+func main() {
+
+	b, err := hex.DecodeString(opts.PubKey)
+
+	key, err := btcec.ParsePubKey(b, btcec.S256())
+
+	fmt.Println(opts.PubKey, len(opts.PubKey), key, err)
+	//client, err := rpcclient.New(&rpcclient.ConnConfig{
+	//	HTTPPostMode: true,
+	//	DisableTLS:   true,
+	//	Host:         "127.0.0.1:8332",
+	//	User:         "testing",
+	//	Pass:         "T4NHKPmdDKQeF1DvZLfbhP6WSNm2oQqv-lzk1WSsbvs=",
+	//}, nil)
+	//if err != nil {
+	//	log.Fatalf("error creating new btc client: %v", err)
+	//}
+	//
+	//// list accounts
+	//info, err := client.GetPeerInfo()
+	//if err != nil {
+	//	log.Fatalf("error getting info: %v", err)
+	//}
+	//
+	//log.Println(info)
+
+}
--- go.mod
+++ go.mod
@@ -1,30 +1,20 @@
 module github.com/meeDamian/bc1toolkit
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fsnotify/fsnotify v1.4.7 // indirect
-	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/btcsuite/btcd v0.0.0-20190410025418-9bfb2ca0346b
 	github.com/gopherjs/gopherjs v0.0.0-20180825215210-0210a2f0f73c // indirect
-	github.com/hpcloud/tail v1.0.0 // indirect
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
+	github.com/lightninglabs/neutrino v0.0.0-20190417030509-c2b6f4508e34 // indirect
+	github.com/lightningnetwork/lnd v0.5.1-beta.0.20190416133022-58ec9efed2a6
 	github.com/mjibson/esc v0.1.0
-	github.com/onsi/ginkgo v1.6.0 // indirect
-	github.com/onsi/gomega v1.4.1 // indirect
 	github.com/pkg/errors v0.8.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.0.6
 	github.com/smartystreets/assertions v0.0.0-20180820201707-7c9eb446e3cf // indirect
 	github.com/smartystreets/goconvey v0.0.0-20180222194500-ef6db91d284a
 	github.com/stretchr/testify v1.2.2 // indirect
-	golang.org/x/crypto v0.0.0-20180904163835-0709b304e793 // indirect
-	golang.org/x/net v0.0.0-20180906233101-161cd47e91fd
-	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
-	golang.org/x/sys v0.0.0-20180909071014-4526dd3c8b56 // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20190206173232-65e2d4e15006
 	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
-	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
-	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
-	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
